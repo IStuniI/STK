c529c8bd38489e36c9c9c693099c33ffd0e68300
Stein <87486463+IStuniI@users.noreply.github.com>
2022-06-22 17:14:38 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fileinput import filename
 import os
 import base64
 import sys
@@ -61,10 +62,11 @@ def update():
                 for file_name in file_names:
                     #overwrite the old files
                     if file_name != "STK":
-                        shutil.move("C:\STK\\bin\STKUP\\"+file_name, "C:\STK\\bin\\STK\\"+file_name)
+                        if filename != ".git":
+                            shutil.move("C:\STK\\bin\STKUP\\"+file_name, "C:\STK\\bin\\STK\\"+file_name)
                 os.system("cls")
                 print(succes+" Successfully updated! Restarting...")
-                os.system(f"python3 {path}\\main.py")
+                os.system(f"python3 {path}\\main.py") 
             elif cmd == "n":
                 print(succes+" Not updating...")
         else:
