d56205aa4b6582fb268c9f9aac84d3abe5985d3c
voiceviewco <constantinvoiceview@gmail.com>
2022-06-22 17:47:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -62,10 +62,14 @@ def update():
                 print(succes+" Downloaded! Cloning...")
                 os.system("rmdir /S /Q C:\STK\\bin\STK")
                 os.system("del /f /s /q C:\STK\\bin\STK")
+                os.system("mkdir C:\STK\\bin\STK\\asset")
                 file_names = os.listdir("C:\STK\\bin\STKUP")
                 for file_name in file_names:
                     if file_name != ".git":
-                        shutil.move("C:\STK\\bin\STKUP\\"+file_name, "C:\STK\\bin\STK")
+                        if not file_name == "config.stk" or file_name == "version.stk" or file_name == "help.stk":
+                            shutil.move("C:\STK\\bin\STKUP\\"+file_name, "C:\STK\\bin\STK")
+                        else:
+                            shutil.copy("C:\STK\\bin\STKUP\\"+file_name, "C:\STK\\bin\STK\\asset")
                         print(succes+" Cloned "+file_name)
                 os.system("cls")
                 print(succes+" Cloned! Cleaning up...")
